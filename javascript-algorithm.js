d8951f0bbd7f6a7b4883d5db2e09cb64a9b9b1fb
Khoa Nguyen Dang <nguyendangkhoa25@gmail.com>
2019-12-30 15:14:58 +0700

    Update javascript-algorithm.js

--- javascript-algorithm.js
+++ javascript-algorithm.js
@@ -1,13 +1,15 @@
 //=================1. Reverse a String(Đảo chuỗi)=========================//
-    function reverseString(str) {
-      //Convert a string to an array
-      let strArr = str.split("");
-      let reverseStr = "";
-      for(let i = strArr.length -1; i >= 0; i--){
-        reverseStr += strArr[i];
-      }
-      return reverseStr;
-    }
+function reverseString(str) {
+  //Convert a string to an array
+  let strArr = str.split("");
+  let reverseStr = "";
+  for(let i = strArr.length -1; i >= 0; i--){
+    reverseStr += strArr[i];
+  }
+  return reverseStr;
+}
+//Testing
+reverseString("Hello");
 
 
 //=================2. Factorialize a number(Giai thừa)=========================//
@@ -18,6 +20,8 @@ function factorialize(num) {
   }
   return facNum;
 }
+//Testing
+factorialize(5);
 
 
 //================3. Convert Celsius to Fahrenheit(Convert độ C sang độ F) ====//
@@ -25,3 +29,41 @@ function convertToF(celsius) {
   let fahrenheit = (celsius * 9/5) + 32;
   return fahrenheit;
 }
+//Testing
+convertToF(30);
+
+//================4. Find the Longest Word in a String =========================//
+function findLongestWordLength(str) {
+  let strArr = str.split(" ");
+  let longestWord = "";
+  for(let i = 0; i < strArr.length; i++){
+    if(longestWord.length < strArr[i].length){
+      longestWord = strArr[i];
+    }
+  }
+  return longestWord.length;
+}
+//Testing
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+
+//================5. Return Largest Numbers in Arrays =========================//
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/return-largest-numbers-in-arrays
+function largestOfFour(arr) {
+  let arrResult = [];
+  for(let i = 0; i < arr.length; i++){
+    let childArr = arr[i];
+    let bigestNum = childArr[0];
+    for(let j =0; j < childArr.length; j++){
+      if(childArr[j] >= bigestNum){
+        bigestNum = childArr[j];
+      }
+    }
+    arrResult.push(bigestNum);
+  }
+  return arrResult;
+}
+
+
+
+//================5.  =========================//
