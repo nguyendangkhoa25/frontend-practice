eca3fcc626bfc7b15d69d8887ae2d3e5431f7f4b
Khoa Nguyen Dang <nguyendangkhoa25@gmail.com>
2020-01-06 22:56:17 +0700

    Update javascript-algorithm.js

--- javascript-algorithm.js
+++ javascript-algorithm.js
@@ -66,4 +66,51 @@ function largestOfFour(arr) {
 
 
 
-//================5.  =========================//
+//================6. Truncate a String =========================//
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/truncate-a-string
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  if(num >= str.length && num > 3){
+    return str.slice(0, num);
+  }else if(num > 3){
+    return str.slice(0, num) + '...';
+  }else{
+    return str.slice(0, num)+ '...';
+  }
+}
+truncateString("Peter Piper picked a peck of pickled peppers", 11)
+//================7. Finders Keepers =========================//
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/finders-keepers
+function findElement(arr, func) {
+  for(let i = 0; i< arr.length; i++){
+    if(func(arr[i])){
+      return arr[i];
+    }
+  }
+  return undefined;
+}
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+//================8. Boo who =========================//
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/boo-who
+function booWho(bool) {
+  return typeof bool === 'boolean';
+}
+
+booWho(null);
+
+//================9. Title Case a Sentence =========================//
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/title-case-a-sentence
+function titleCase(str) {
+  str = str.toLowerCase();
+  let arrStr = str.split(' ');
+  for(let i = 0; i < arrStr.length; i ++){
+    //Upper case the first letter of each word
+    arrStr[i] = arrStr[i].charAt(0).toUpperCase() + arrStr[i].slice(1); 
+  }
+  //Join the array to string
+  return arrStr.join(' ');
+}
+//================10.  =========================//
+//================11.  =========================//
+
