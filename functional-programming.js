67a86a7770e2c3ff85db82c5a61d7863f3624275
Khoa Nguyen Dang <nguyendangkhoa25@gmail.com>
2020-01-08 04:09:09 +0700

    create a file to store functional programming practice

---
+++ functional-programming.js
@@ -0,0 +1,14 @@
+/*
+All the functional programming challenges on the freeCodeCamp website will be stored right here.
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/functional-programming/
+*/
+//=================1. Learn About Functional Programming =========================//
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/functional-programming/learn-about-functional-programming
+
+//=================1. =========================//
+//=================1. =========================//
+//=================1. =========================//
+//=================1. =========================//
+//=================1. =========================//
+//=================1. =========================//
+//=================1. =========================//
