b680e4191e4ea3f9ffea694bb89b7123a41bfa1d
Khoa Nguyen Dang <nguyendangkhoa25@gmail.com>
2020-01-20 17:12:42 +0700

    first commit

---
+++ intermediate-algorithm-scripting.js
@@ -0,0 +1,15 @@
+/**
+All the intermediate algorithm javascript challenges on the freeCodeCamp website will be stored right here.
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/
+**/
+//=================1. Sum All Numbers in a Range =========================//
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-numbers-in-a-range
+
+//=================2.  =========================//
+//=================3.  =========================//
+//=================4.  =========================//
+//=================5.  =========================//
+//=================6.  =========================//
+//=================7.  =========================//
+//=================8.  =========================//
+//=================9.  =========================//
